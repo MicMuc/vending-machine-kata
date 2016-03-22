f57a9380155b66c224c1f201927f134f2e442307
Michael McMullen <Michael McMullen>
2016-03-22 12:41:58 -0400

    Multiple coins now increase the total value.

--- src/PillarVendingMachine.java
+++ src/PillarVendingMachine.java
@@ -1,9 +1,11 @@
 public class PillarVendingMachine {
 
+    int currentAmount = 0;
+
 
     public int acceptCoin(String size, String weight) {
         Coin coin = Coin.getCoinByAttributes(size, weight);
 
-        return coin.getValue();
+        return currentAmount += coin.getValue();
     }
 }
--- test/PillarVendingMachineTest.java
+++ test/PillarVendingMachineTest.java
@@ -1,25 +1,37 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class PillarVendingMachineTest {
 
+    PillarVendingMachine pillarVendingMachine;
+
+    @Before
+    public void setup() {
+        pillarVendingMachine = new PillarVendingMachine();
+    }
+
     @Test
     public void testVendingMachineCanAcceptDimes(){
-        PillarVendingMachine pillarVendingMachine = new PillarVendingMachine();
         int currentValue = pillarVendingMachine.acceptCoin("small", "light");
         assertEquals(10, currentValue);
     }
     @Test
     public void testVendingMachineCanAcceptNickels(){
-        PillarVendingMachine pillarVendingMachine = new PillarVendingMachine();
         int currentValue = pillarVendingMachine.acceptCoin("medium", "medium");
         assertEquals(5, currentValue);
     }
     @Test
     public void testVendingMachineCanAcceptQuarters(){
-        PillarVendingMachine pillarVendingMachine = new PillarVendingMachine();
         int currentValue = pillarVendingMachine.acceptCoin("big", "heavy");
         assertEquals(25, currentValue);
+    }
+
+    @Test
+    public void testVendingMachineCanTakeMultipleCoins(){
+        pillarVendingMachine.acceptCoin("small", "light");
+        int currentValue = pillarVendingMachine.acceptCoin("small", "light");
+        assertEquals(20, currentValue);
     }
 }
