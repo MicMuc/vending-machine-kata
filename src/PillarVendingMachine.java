4513b40f1f6bf45c397b7c0987fa54923803d4aa
Michael McMullen <Michael McMullen>
2016-03-22 11:59:52 -0400

    Now makes sure nickels, dimes, and quarters are accepted.

--- src/PillarVendingMachine.java
+++ src/PillarVendingMachine.java
@@ -2,6 +2,15 @@ public class PillarVendingMachine {
 
 
     public int acceptCoin(String coin) {
-        return 10;
+        if(coin.equals("nickel")){
+            return 5;
+        }
+        if(coin.equals("dime")){
+            return 10;
+        }
+        if(coin.equals("quarter")){
+            return 25;
+        }
+        return 0;
     }
 }
--- test/PillarVendingMachineTest.java
+++ test/PillarVendingMachineTest.java
@@ -4,10 +4,22 @@ import static org.junit.Assert.assertEquals;
 
 public class PillarVendingMachineTest {
 
-@Test
-public void testVendingMachineCanAcceptCoins(){
-    PillarVendingMachine pillarVendingMachine = new PillarVendingMachine();
-    int currentValue = pillarVendingMachine.acceptCoin("dime");
-    assertEquals(10, currentValue);
-}
+    @Test
+    public void testVendingMachineCanAcceptDimes(){
+        PillarVendingMachine pillarVendingMachine = new PillarVendingMachine();
+        int currentValue = pillarVendingMachine.acceptCoin("dime");
+        assertEquals(10, currentValue);
+    }
+    @Test
+    public void testVendingMachineCanAcceptNickels(){
+        PillarVendingMachine pillarVendingMachine = new PillarVendingMachine();
+        int currentValue = pillarVendingMachine.acceptCoin("nickel");
+        assertEquals(5, currentValue);
+    }
+    @Test
+    public void testVendingMachineCanAcceptQuarters(){
+        PillarVendingMachine pillarVendingMachine = new PillarVendingMachine();
+        int currentValue = pillarVendingMachine.acceptCoin("quarter");
+        assertEquals(25, currentValue);
+    }
 }
